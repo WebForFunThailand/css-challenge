6b75a3345579231aeb21757f5b4fddbc43481019
lagmanzaza <lagmanmc@gmail.com>
2020-12-18 22:59:14 +0700

    chore: init

---
+++ src/pages/summary.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react';
+import Head from 'next/head';
+
+import Base from '@/components/base';
+
+const Summary: FunctionComponent = () => (
+  <div className="container">
+    <Head>
+      <title>Summary</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <Base />
+    </main>
+  </div>
+);
+
+export default Summary;
