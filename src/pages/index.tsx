5fb608756fcee7ae0cea5a47b0b966ef903f7307
lagmanzaza <lagmanmc@gmail.com>
2020-12-21 14:42:30 +0700

    feat: main layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import Head from 'next/head';
-
+import { MainLayout } from '@/layouts/MainLayout';
 import Base from '@/components/base';
 
 const Home: FunctionComponent = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+  <MainLayout>
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <main>
-      <Base />
-    </main>
-  </div>
+      <main>
+        <Base />
+      </main>
+    </div>
+  </MainLayout>
 );
 
 export default Home;
