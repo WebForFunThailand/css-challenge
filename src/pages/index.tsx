e621b61a7457d0f7e9fa99788bfcdb509466f22b
lagmanzaza <lagmanmc@gmail.com>
2020-12-27 00:31:23 +0700

    feat: device resize event

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
+import { useState, useEffect } from 'react';
+import { NextPage } from 'next';
 import Head from 'next/head';
 import { jsx, css } from '@emotion/react';
 import { useRouter } from 'next/router';
-import { MainLayout } from '@/layouts/MainLayout';
-import { Header } from '@/components/Header';
-import { NextPage } from 'next';
-import { baseButtonStyle, challengeButtonStyle } from '@/components/button';
-import { center } from '@/components/style-helper';
+import { center } from '../components/style-helper';
+import { MainLayout } from '../layouts/MainLayout';
+import { MobileLayout } from '../layouts/MobileLayout';
+import { Header } from '../components/Header';
+import { baseButtonStyle, challengeButtonStyle } from '../components/button';
 
+const IPAD_PRO_WIDTH = 1366;
 // style
 const subTtitleStyle = css`
   color: #555555;
@@ -19,10 +22,65 @@ const subTtitleStyle = css`
 
 const Home: NextPage = () => {
   const router = useRouter();
+  const [isMobile, setIsMobile] = useState(false);
+
+  const onChangeDeviceSize = (height: number, width: number) => {
+    if (height > width) {
+      if (width <= IPAD_PRO_WIDTH) {
+        setIsMobile(true);
+        return;
+      }
+    } else if (width <= IPAD_PRO_WIDTH) {
+      setIsMobile(true);
+      return;
+    }
+
+    setIsMobile(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener(`resize`, () => {
+      onChangeDeviceSize(window.innerHeight, window.innerWidth);
+    });
+
+    onChangeDeviceSize(window.innerHeight, window.innerWidth);
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    return () => window.removeEventListener(`resize`, () => {});
+  }, []);
 
   const onClickToChallenge = () => {
     router.push(`/challenge`);
   };
+
+  if (isMobile) {
+    return (
+      <MobileLayout>
+        <div css={subTtitleStyle}>
+          <div>
+            <br />
+            <iframe
+              title="learning-youtube"
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/_0s8qXRCY4Y"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            />
+          </div>
+          <p
+            css={css`
+              margin-top: 1em;
+              text-align: center;
+              color: #ff0000;
+              font-size: 16px;
+            `}
+          >
+            เกมนี้ไม่รองรับการเล่นบนโทรศัพท์มือถือโปรดเปิดเล่นบนคอมพิวเตอร์
+          </p>
+        </div>
+      </MobileLayout>
+    );
+  }
 
   return (
     <MainLayout>
@@ -38,11 +96,12 @@ const Home: NextPage = () => {
             <div>
               <br />
               <iframe
+                title="learning-youtube"
                 width="100%"
                 height="315"
                 src="https://www.youtube.com/embed/_0s8qXRCY4Y"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              ></iframe>
+              />
             </div>
             <p
               css={css`
