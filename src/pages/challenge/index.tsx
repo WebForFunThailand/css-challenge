9676b35622560113660480d6630a3b5e6ac8db02
Suthep Chanchuphol <suthep.chanchuphol@outlook.com>
2020-12-26 23:09:09 +0700

    feat: add image diffing
    # Conflicts:
    #	src/pages/challenge/index.tsx

--- src/pages/challenge/index.tsx
+++ src/pages/challenge/index.tsx
@@ -13,6 +13,7 @@ import {
   skipButtonStyle,
   submitButtonStyle,
 } from '@/components/button';
+import pixelmatch from 'pixelmatch';
 import {
   EditorLayout,
   ExpectedResultSection,
@@ -55,6 +56,7 @@ const Challenge = () => {
 
   // Convert User Div into Canvas
   const userDiv = useRef(null);
+  const expectedImage = useRef(null);
   const resultImage = useRef(null);
   useEffect(() => useDomToImage(userDiv.current, resultImage.current), [
     userCss,
@@ -75,6 +77,42 @@ const Challenge = () => {
     { id: 7, status: `idle` },
   ];
 
+  // Diff
+  const DIMENSION = 500;
+  const TOTAL_PIXEL = DIMENSION * DIMENSION;
+  const roundTo2Decimals = (num) =>
+    Math.round((num + Number.EPSILON) * 100) / 100;
+  const diffImage = () => {
+    // Draw Expected
+    const expectedCanvas = document.createElement(`canvas`);
+    expectedCanvas.width = DIMENSION;
+    expectedCanvas.height = DIMENSION;
+    const expectedCanvasCtx = expectedCanvas.getContext(`2d`);
+    expectedCanvasCtx.drawImage(expectedImage.current, 0, 0);
+
+    // Draw User
+    const userCanvas = document.createElement(`canvas`);
+    userCanvas.width = DIMENSION;
+    userCanvas.height = DIMENSION;
+    const userCanvasCtx = userCanvas.getContext(`2d`);
+    userCanvasCtx.drawImage(resultImage.current, 0, 0);
+
+    // Diff
+    const diffPixels = pixelmatch(
+      expectedCanvasCtx.getImageData(0, 0, DIMENSION, DIMENSION).data,
+      userCanvasCtx.getImageData(0, 0, DIMENSION, DIMENSION).data,
+      null,
+      DIMENSION,
+      DIMENSION,
+      { threshold: 0.1 },
+    );
+    const samePercent = roundTo2Decimals(
+      100 - (diffPixels * 100) / TOTAL_PIXEL,
+    );
+    // eslint-disable-next-line no-console
+    console.log(`Same ${samePercent}%`);
+  };
+
   return (
     <MainLayout>
       <div>
@@ -89,17 +127,14 @@ const Challenge = () => {
             <div css={[DisplaySection, ExpectedResultSection]}>
               <span>Expected Result</span>
               <img
-                src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                ref={expectedImage}
+                src="/img/blank.png"
                 alt="Expected result"
               />
             </div>
             <div css={[DisplaySection, UserResultSection]}>
               <span>Your Result</span>
-              <img
-                ref={resultImage}
-                src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-                alt="Your result"
-              />
+              <img ref={resultImage} src="/img/blank.png" alt="Your result" />
               {/* rendering area */}
               <style
                 // eslint-disable-next-line react/no-danger
@@ -141,7 +176,11 @@ const Challenge = () => {
             <button css={[baseButtonStyle, skipButtonStyle]} type="button">
               Skip
             </button>
-            <button css={[baseButtonStyle, submitButtonStyle]} type="button">
+            <button
+              css={[baseButtonStyle, submitButtonStyle]}
+              type="button"
+              onClick={diffImage}
+            >
               Submit
             </button>
           </section>
