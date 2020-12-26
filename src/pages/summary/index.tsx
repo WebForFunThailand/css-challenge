34d0734eff8e1b5b31abcf62886d1dfb7d66af35
Ming <mingbackmountain@gmail.com>
2020-12-26 22:39:40 +0700

    fix common layout

--- src/pages/summary/index.tsx
+++ src/pages/summary/index.tsx
@@ -15,8 +15,6 @@ const Summary: FunctionComponent = () => (
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <img alt="graph" src="./graph.svg" />
-
         <MessageBox
           title="Web For Fun #2 is now open for registration!"
           description="10 Minutes CSS Challenge is a game developed for the developer who has writing skills in CSS or interested in it. For this challenge, you’ll get an image and your job is to write CSS to be exactly same design as that image."
