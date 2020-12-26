14ec5aff0b88ee615d2e15d642af60da63ca8e9f
chunza2542 <hello@thechun.dev>
2020-12-26 23:07:00 +0700

    Update button style

--- src/components/button.ts
+++ src/components/button.ts
@@ -22,6 +22,15 @@ export const skipButtonStyle = css`
   }
 `;
 
+export const submitButtonStyle = css`
+  color: rgb(14, 104, 248);
+  background: rgba(14, 104, 248, 0.1);
+
+  &:hover {
+    background: rgba(14, 104, 248, 0.17);
+  }
+`;
+
 export const facebookButtonStyle = css`
   color: rgb(14, 104, 248);
   background: rgba(14, 104, 248, 0.1);
