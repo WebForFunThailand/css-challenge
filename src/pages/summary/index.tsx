aa3d4d0f4d87c48836eb613fa6fa6204dd4761b2
lagmanzaza <lagmanmc@gmail.com>
2020-12-28 20:49:58 +0700

    webp

--- src/pages/summary/index.tsx
+++ src/pages/summary/index.tsx
@@ -58,7 +58,7 @@ const Summary: NextPage = () => (
         />
         <meta
           property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          content="https://webforfun.dev/static/40ea6582af50f3afb237931d3dd76e72/47203/logo.webp"
         />
       </Head>
       <main>
