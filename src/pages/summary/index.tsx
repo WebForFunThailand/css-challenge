a2a16690b894be0935741a20cfca6c022da04bae
lagmanzaza <lagmanmc@gmail.com>
2020-12-28 21:01:14 +0700

    refator: seo meta tag

--- src/pages/summary/index.tsx
+++ src/pages/summary/index.tsx
@@ -43,47 +43,57 @@ const graphAxis = css`
   }
 `;
 
-const Summary: NextPage = () => (
-  <MainLayout>
-    <div className="container">
-      <Head>
-        <title>Summary</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
-        <meta property="og:title" content="You are CSS Wizard" />
-        <meta
-          property="og:description"
-          content="10 Minutes CSS Challenge is a game developed for the developer who has writing skills in CSS or interested in it. For this challenge, you’ll get an image and your job is to write CSS to be exactly same design as that image."
-        />
-        <meta
-          property="og:image"
-          content="https://webforfun.dev/static/40ea6582af50f3afb237931d3dd76e72/47203/logo.webp"
-        />
-      </Head>
-      <main>
-        <MessageBox
-          title="Web For Fun #2 is now open for registration!"
-          description="10 Minutes CSS Challenge is a game developed for the developer who has writing skills in CSS or interested in it. For this challenge, you’ll get an image and your job is to write CSS to be exactly same design as that image."
-        />
-        <p css={badgeLabelStyle}>
-          You’re a <span css={badgeStyle}>CSS Wizard</span>
-        </p>
-        <div
-          css={css`
-            position: relative;
-            transform: scale(0.9);
-          `}
-        >
-          <img alt="graph" src="./graph.svg" css={graphStyle} />
-          <div css={graphAxis}>Accuracy</div>
-          <div css={graphAxis}>Time</div>
-          <UserScore time="9:10" percentage={88} />
-        </div>
-        <SummaryMessageBox rankPercentage={11} time="11:30" percentage={75} />
-      </main>
-    </div>
-  </MainLayout>
-);
+const Summary: NextPage = () => {
+  const seoInfo = {
+    title: `You are CSS Wizard`,
+    description: `10 Minutes CSS Challenge is a game developed for the developer who has writing skills in CSS or interested in it. For this challenge, you’ll get an image and your job is to write CSS to be exactly same design as that image.`,
+    image: `https://webforfun.dev/static/40ea6582af50f3afb237931d3dd76e72/47203/logo.webp`,
+    cardLabel: `CSS Challenge Web for fun`,
+    website: `https://webforfun.dev/`,
+  };
+
+  return (
+    <MainLayout>
+      <div className="container">
+        <Head>
+          <title>Summary</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={seoInfo.website} />
+          <meta property="og:title" content={seoInfo.title} />
+          <meta property="og:description" content={seoInfo.description} />
+          <meta property="og:image" content={seoInfo.image} />
+
+          <meta property="twitter:card" content={seoInfo.cardLabel} />
+          <meta property="twitter:url" content={seoInfo.website} />
+          <meta property="twitter:title" content={seoInfo.title} />
+          <meta property="twitter:description" content={seoInfo.description} />
+          <meta property="twitter:image" content={seoInfo.image} />
+        </Head>
+        <main>
+          <MessageBox
+            title="Web For Fun #2 is now open for registration!"
+            description="10 Minutes CSS Challenge is a game developed for the developer who has writing skills in CSS or interested in it. For this challenge, you’ll get an image and your job is to write CSS to be exactly same design as that image."
+          />
+          <p css={badgeLabelStyle}>
+            You’re a <span css={badgeStyle}>CSS Wizard</span>
+          </p>
+          <div
+            css={css`
+              position: relative;
+              transform: scale(0.9);
+            `}
+          >
+            <img alt="graph" src="./graph.svg" css={graphStyle} />
+            <div css={graphAxis}>Accuracy</div>
+            <div css={graphAxis}>Time</div>
+            <UserScore time="9:10" percentage={88} />
+          </div>
+          <SummaryMessageBox rankPercentage={11} time="11:30" percentage={75} />
+        </main>
+      </div>
+    </MainLayout>
+  );
+};
 
 export default Summary;
